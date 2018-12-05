442dc3c16f3e68220b457515c7466ebb3e91b4b0
Alice Huston <shuston@stevens.edu>
2018-12-05 15:46:27 -0500

    freshly commented code for your visual enjoyment

--- Swerve_test.ino
+++ Swerve_test.ino
@@ -1,18 +1,29 @@
-#include <Servo.h>
+a#include <Servo.h>
 #include <WemosInit.h>
 
+/*Pin Definitions: 
+ * Echo/Trig belong to the ultrasonic sensor
+ * m1 and m2 belong to motor 1 (left) and motor 2 (right) respectively
+ */
 #define Echo D5
 #define Trig D4 
 #define m2   D2
 #define m1   D3
+
+//initialize the motor objects
 Servo motor1;
 Servo motor2;
-int state = 0; // 0 = forward, 1 = turning
-bool debug = true;
-int lStop = 82;
-int rStop = 90;
+
+enum states {STRAIGHT, TURN}; //all possible things the bot could do
+int phase = 0; //represents what state the bot is currently in
+bool debug = true; //if true: prints states to the serial monitor
+int lStop = 82; //"stops" trimmed for drift so the bot drives straight
+int rStop = 90; 
 int throttle = 29;
 
+/*Initialization procedure:
+ * Setup motors, stop the bot from moving, connect any sensors, setup debugging
+ */
 void setup() {
   motor1.attach(m1);
   motor2.attach(m2);
@@ -24,33 +35,31 @@ void setup() {
 }
 
 void loop() {
-  if(ultrasonicPing(Trig,Echo) < 900){ //check if obstacle is roughly 4in away
-    state = 1;
+  if(ultrasonicPing(Trig,Echo) < 900){ //check if obstacle is roughly 4in away, start turning procedure if true
+    phase = TURN;
   }
   else{
-    state = 0;
+    phase = STRAIGHT;
   }
-  if (state == 1) { //if its close, go right, then straight, then left
-    int t = 1500;
+  if (phase == TURN) { //if an object close, go right, then straight, then left
+    int t = 1500; //default unit of time to uniformly allow bot to turn, adjust variable for changing turn radius
     motor1.write(97);
-    motor2.write(97);
+    motor2.write(97); //turn right
     delay(t);
     motor1.write(rStop - throttle);
-    motor2.write(lStop + throttle);
+    motor2.write(lStop + throttle); //go straight
     delay(t/2);
     motor1.write(86);
-    motor2.write(86);
+    motor2.write(86); //turn left
     delay(t);
     motor1.write(rStop - throttle);
-    motor2.write(lStop + throttle);
+    motor2.write(lStop + throttle); //go straight
     if(debug){Serial.println("swerve");}
   }
-  else if(state == 0){ //otherwise go straight
+  else if(phase == STRAIGHT){ //otherwise go straight
     motor1.write(rStop - throttle);
     motor2.write(lStop + throttle);
     if(debug){Serial.println("moving forward");}
   }
   if(debug){Serial.println(ultrasonicPing(Trig,Echo));}
 }
-
-
